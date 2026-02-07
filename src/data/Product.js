bc62f744daa293ba4c1e8e3a1fc5e1d5b69aa115
SamuBerserk30 <samuelsotocdn@gmail.com>
2026-02-06 20:30:47 -0500

    Agregando estilo a los productos en ProductCard.module.css

--- src/data/Product.js
+++ src/data/Product.js
@@ -5,30 +5,30 @@ export const Product = [
     category: 'Laptops',
     price: 2499.99,
     image: '',
-    descripcion: 'Laptop potente con procesador Intel Core i5, 16GB de RAM y almacenamiento SSD de 512GB. Ideal para tareas exigentes y juegos.'
+    description: 'Laptop potente con procesador Intel Core i5, 16GB de RAM y almacenamiento SSD de 512GB. Ideal para tareas exigentes y juegos.'
   },
  {
     id: 2,
     name: 'Smartphone Samsung Galaxy S21',
     category: 'Smartphones',
     price: 3500000,
-    Image: '',
-    descripcion: 'Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas, cámara de alta resolución y batería de larga duración. Perfecto para capturar momentos y disfrutar de contenido multimedia.'  
+    image: '',
+    description: 'Smartphone Samsung Galaxy S21 con pantalla AMOLED de 6.2 pulgadas, cámara de alta resolución y batería de larga duración. Perfecto para capturar momentos y disfrutar de contenido multimedia.'  
  },
  {
     id: 3,
     name: 'Auriculares Sony WH-1000XM4',
     category: 'Audio',
     price: 1200000,
-    Image: '',
-    descripcion: 'Audifonos Sony WH-1000XM4 con cancelación de ruido, sonido de alta calidad y batería de larga duración. Perfectos para música y llamadas.'
+    image: '',
+    description: 'Audifonos Sony WH-1000XM4 con cancelación de ruido, sonido de alta calidad y batería de larga duración. Perfectos para música y llamadas.'
  },
  {
     id: 4,
     name: 'Smartwatch Apple Watch Series 6',
     category: 'Wearables',
     price: 2500000,
-    Image: '',
-    descripcion: 'Smartwatch Apple Watch Series 6 con monitoreo de salud, GPS integrado y resistencia al agua. Ideal para mantenerse conectado y activo durante todo el día.'
+    image: '',
+    description: 'Smartwatch Apple Watch Series 6 con monitoreo de salud, GPS integrado y resistencia al agua. Ideal para mantenerse conectado y activo durante todo el día.'
  }
 ]
