90e0d7d489f8962efc0bb0c8a6e4cd7134a211de
Alejocr02 <alejocardona0@gmail.com>
2026-03-27 14:18:50 -0500

    configurando y arreglando errores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,14 @@ import styles from "./Header.module.css";
 
 import Navbar from "./Navbar";
 
-function Header({ activePage, onNavigate, user, onSignIn, onSignOut }) {
+function Header({ user, onSignIn, onSignOut, cartItemCount }) {
   return (
     <header className={styles.header}>
       <Navbar
-        activePage={activePage}
-        onNavigate={onNavigate}
         user={user}
         onSignIn={onSignIn}
         onSignOut={onSignOut}
+        cartItemCount={cartItemCount}
       />
     </header>
   );
